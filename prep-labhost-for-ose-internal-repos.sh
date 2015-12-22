dec27ccf662cb841f8d213b1b6269c9453d17bd4
Thomas Qvarnström <tqvarnst@redhat.com>
2015-12-22 23:44:34 +0100

    Update prep-labhost-for-ose-internal-repos.sh

--- prep-labhost-for-ose-internal-repos.sh
+++ prep-labhost-for-ose-internal-repos.sh
@@ -31,7 +31,7 @@ name=latest-EXTRAS-7-RHEL-7
 baseurl=http://download.eng.bos.redhat.com/devel/candidates/latest-EXTRAS-7-RHEL-7/compose/Server/x86_64/os
 enabled=1
 gpgcheck=0
-skip_if_unavailable=1" > /etc/yum.repos.d/beaker-Server-extras.repo
+skip_if_unavailable=1" > /etc/yum.repos.d/latest-EXTRAS-7-RHEL-7.repo
 
 yum clean all
 
