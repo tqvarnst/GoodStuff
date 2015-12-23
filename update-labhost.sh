25abb9b362bf9aa06c8f6c18005196cbb1f49ead
Thomas Qvarnström <tqvarnst@redhat.com>
2015-12-23 14:17:11 +0100

    Update update-labhost.sh

--- update-labhost.sh
+++ update-labhost.sh
@@ -13,14 +13,14 @@ skip_if_unavailable=1
 
 [rhel-7-server-extras-rpms]
 name=Red Hat Enterprise Linux 7 Server - Extras (RPMs)
-baseurl=baseurl=http://hpc-dl320a-01.mw.lab.eng.bos.redhat.com/repos/rhel/server/7/7Server/$basearch/extras/os
+baseurl=http://hpc-dl320a-01.mw.lab.eng.bos.redhat.com/repos/rhel/server/7/7Server/$basearch/extras/os
 enabled=1
 gpgcheck=0
 skip_if_unavailable=1
 
 [rhel-7-server-ose-3.1-rpms]
 name=Red Hat OpenShift Enterprise 3.1 (RPMs)
-baseurl=baseurl=http://hpc-dl320a-01.mw.lab.eng.bos.redhat.com/repos/rhel/server/7/7Server/$basearch/ose/3.1/os
+baseurl=http://hpc-dl320a-01.mw.lab.eng.bos.redhat.com/repos/rhel/server/7/7Server/$basearch/ose/3.1/os
 enabled=1
 gpgcheck=0
 skip_if_unavailable=1
